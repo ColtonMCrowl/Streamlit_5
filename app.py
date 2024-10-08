b3b9c78d4d38699f8838436b02cf1f7d831e1975
ColtonMCrowl <122884963+ColtonMCrowl@users.noreply.github.com>
2024-10-08 11:55:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,15 @@ import streamlit as st
 
 st.title("My Streamlit App")
 
-col1, col2 = 
+# 2 columns to display text.
+col1, col2 = st.columns(2)
+
+#Column 1 with the text "Hello"
+col1.write("Hello")
+
+#Column 2 with the text "World"
+col2.write("World")
+
+#An expander that reveals text when clicked
+with st.expander("Click me"):
+    st.write("Here you could put in some really, really explanatory stuff.")
